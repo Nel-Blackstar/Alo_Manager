54e02f46efd838f08211e772cc88bf42ed9ae543
LutherFirst <lutherfabrice@gmail.com>
2019-04-26 14:48:24 +0100

    premier commit de alo manager

---
+++ src/main/java/com/alo/AloManager/AloManagerApplication.java
@@ -0,0 +1,13 @@
+package com.alo.AloManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AloManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AloManagerApplication.class, args);
+	}
+
+}
