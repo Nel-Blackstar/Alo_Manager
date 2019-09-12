48c12ff61ae8cbd33bccbc21bf8fbcf816c7ed1c
Kamgue fokoua blondin <fokouak@gmail.com>
2019-09-12 12:31:50 +0200

    mise en Récuperation de la partie du black

--- src/main/java/com/live/paie/entities/BulletinPaie.java
+++ src/main/java/com/live/paie/entities/BulletinPaie.java
@@ -9,9 +9,7 @@ import javax.persistence.ManyToOne;
 @Entity
 public class BulletinPaie extends LiveEntity {
     private float nbreHeureTravaillees;
-    private float nbreHeureSup50;
-    private float nbreHeureSup100;
-    private float nbreHeureSup25;
+    private String description;
     private Long nbreJoursFeries;
     private Long nbreCongesPayes;
     @ManyToOne(optional = true, targetEntity = Personnel.class)
@@ -20,46 +18,24 @@ public class BulletinPaie extends LiveEntity {
     public BulletinPaie() {
     }
 
-    public BulletinPaie(float nbreHeureTravaillees, float nbreHeureSup50, float nbreHeureSup100, float nbreHeureSup25,
-                        Long nbreJoursFeries, Long nbreCongesPayes) {
-        this.nbreHeureTravaillees = nbreHeureTravaillees;
-        this.nbreHeureSup50 = nbreHeureSup50;
-        this.nbreHeureSup100 = nbreHeureSup100;
-        this.nbreHeureSup25 = nbreHeureSup25;
-        this.nbreJoursFeries = nbreJoursFeries;
-        this.nbreCongesPayes = nbreCongesPayes;
-    }
+    public BulletinPaie(float nbreHeureTravaillees, String description, Long nbreJoursFeries, Long nbreCongesPayes,
+			Personnel personnel) {
+		super();
+		this.nbreHeureTravaillees = nbreHeureTravaillees;
+		this.description = description;
+		this.nbreJoursFeries = nbreJoursFeries;
+		this.nbreCongesPayes = nbreCongesPayes;
+		this.personnel = personnel;
+	}
 
-    public float getNbreHeureTravaillees() {
+
+
+	public float getNbreHeureTravaillees() {
         return nbreHeureTravaillees;
     }
 
     public void setNbreHeureTravaillees(float nbreHeureTravaillees) {
         this.nbreHeureTravaillees = nbreHeureTravaillees;
-    }
-
-    public float getNbreHeureSup50() {
-        return nbreHeureSup50;
-    }
-
-    public void setNbreHeureSup50(float nbreHeureSup50) {
-        this.nbreHeureSup50 = nbreHeureSup50;
-    }
-
-    public float getNbreHeureSup100() {
-        return nbreHeureSup100;
-    }
-
-    public void setNbreHeureSup100(float nbreHeureSup100) {
-        this.nbreHeureSup100 = nbreHeureSup100;
-    }
-
-    public float getNbreHeureSup25() {
-        return nbreHeureSup25;
-    }
-
-    public void setNbreHeureSup25(float nbreHeureSup25) {
-        this.nbreHeureSup25 = nbreHeureSup25;
     }
 
     public Long getNbreJoursFeries() {
@@ -74,7 +50,15 @@ public class BulletinPaie extends LiveEntity {
         return nbreCongesPayes;
     }
 
-    public void setNbreCongesPayes(Long nbreCongesPayes) {
+    public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setNbreCongesPayes(Long nbreCongesPayes) {
         this.nbreCongesPayes = nbreCongesPayes;
     }
 
